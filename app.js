785932a5e5631f4cf93a4416f75e0bf1614a3baa
SulbaranDev <angeldv.sulbaran@gmail.com>
2022-10-23 00:43:00 +0200

    PicGrid App

---
+++ app.js
@@ -0,0 +1,90 @@
+const auth = "563492ad6f91700001000001110e41e4145f42eab82385bbbde4bf14"
+const gallery = document.querySelector('.gallery');
+const searchInput = document.querySelector('.search-input');
+const form = document.querySelector('.search-form');
+const more = document.querySelector('.more');
+let searchValue;
+let page = 1;
+let fetchLink;
+let currentSearch;
+//event Listener
+
+searchInput.addEventListener('input', updateInput);
+form.addEventListener('submit', (e)=>{
+    searchPhotos(searchValue)
+    currentSearch = searchValue;
+    e.preventDefault()
+
+})
+
+more.addEventListener('click', loadMore)
+
+
+
+function updateInput(e){
+    searchValue = e.target.value
+}
+
+
+
+async function fetchApi(url){
+  const datafetch = await fetch(url, {
+      method: "GET",
+      headers:{
+          Accept: "application/json",
+          Authorization: auth
+      }
+  });
+  const data = await datafetch.json();
+  return data;
+}
+
+function generatePictures(data){
+    data.photos.forEach(photo=>{
+        const galleryImg = document.createElement('div');
+        galleryImg.classList.add('gallery-img');
+        galleryImg.innerHTML = `<img src=${photo.src.large}> </img>
+        <div class"gallery-info">
+        <p>${photo.photographer}</p>
+        <a href=${photo.src.original}>Download</a>
+       </div>
+        `
+       ;
+        gallery.appendChild(galleryImg)
+    })
+}
+async function curatedPhotos(){
+    fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1"
+    const data = await fetchApi(fetchLink);
+    generatePictures(data);
+};
+
+
+
+async function searchPhotos(query){
+    clear();
+    fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
+    const data = await fetchApi(fetchLink)
+    generatePictures(data);
+}
+
+async function loadMore(){
+    page++;
+    if(currentSearch){
+        fetchLink = (`https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page=15&page=${page}`)
+
+    }else{
+        fetchLink = (`https://api.pexels.com/v1/curated?per_page=15&page=${page}`)
+    }
+    const data = await fetchApi(fetchLink);
+    generatePictures(data);
+
+};
+
+function clear(){
+    gallery.innerHTML = "";
+    searchInput.value = "";
+}
+
+
+curatedPhotos();
